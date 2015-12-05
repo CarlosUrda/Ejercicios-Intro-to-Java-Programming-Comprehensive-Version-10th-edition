4cca38ed3f6bb369af803167ebed59a23b9de03a
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2015-12-05 22:33:24 +0100

    Finalización de todos los ejercicios del Capítulo 1.

---
+++ Capitulo 1/Ejercicio13.java
@@ -0,0 +1,25 @@
+
+/**
+ * Obtener la velocidad media de un corredor.
+ *
+ * @author Carlos A. Gómez Urda
+ * @version 1.0
+ */
+public class Ejercicio13
+{
+	public static void main( String args[])
+	{
+		final double a = 3.4;
+		final double b = 50.2;
+		final double e = 44.5;
+		final double c = 24;
+		final double d = 24;
+		final double f = 24;
+		
+		double x = (e*d - b*f) / (a*d - b*c);
+		double y = (a*f - e*c) / (a*d - b*c);
+
+		System.out.println("X: " + x);
+		System.out.println("Y: " + y);
+	}
+}
