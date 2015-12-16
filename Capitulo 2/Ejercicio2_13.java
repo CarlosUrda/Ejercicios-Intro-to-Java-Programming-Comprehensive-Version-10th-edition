9ebdd6a08247e6c0d4cf81151b49c864d1185ea4
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2015-12-16 10:37:59 +0100

    Implementación de los Ejercicios 13 y 14 del Capítulo 2

---
+++ Capitulo 2/Ejercicio2_13.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+/**
+ * Ejercicio 13 del Capítulo 2: Calcular la cantidad de dinero guardada en el
+ * banco después de ir ingresando todos los meses una cantidad y tras haberse
+ * aplicado cada mes los intereses correspondientes a partir del interés anual.
+ *
+ * @author Carlos A. Gómez Urda
+ * @version 1.0
+ */
+public class Ejercicio2_13
+{
+	public static void main( String args[])
+	{
+	    Scanner entrada = new Scanner( System.in).useLocale( Locale.ENGLISH);
+
+        System.out.print( "Introduce la cantidad a ingresar cada mes: ");
+        final double ingresoMensual = entrada.nextDouble();
+
+        System.out.print( "Introduce el interés anual en procentaje: ");
+        final double interesAnual = entrada.nextDouble();
+        final double interesMensual = interesAnual / 1200;
+
+        System.out.print( "Introduce el número de meses a calcular: ");
+        final double meses = entrada.nextDouble();
+   
+        double cuenta = 0;
+        for (int i = 0; i < meses; i++)
+        {
+            cuenta += ingresoMensual;
+            cuenta *= 1 + interesMensual;
+        }
+
+        System.out.println( "Cuenta bancaria: " + cuenta);
+	}
+}
---
+++ Capitulo 2/Ejercicio2_14.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/**
+ * Ejercicio 14 del Capítulo 2: Calcular el índice de masa corporal.
+ *
+ * @author Carlos A. Gómez Urda
+ * @version 1.0
+ */
+public class Ejercicio2_14
+{
+	public static void main( String args[])
+	{
+        Locale.setDefault( Locale.ENGLISH);
+	    Scanner entrada = new Scanner( System.in);
+
+        System.out.print( "Introduce el peso en libras: ");
+        final double pesoLibras = entrada.nextDouble();
+        final double pesoKg = pesoLibras * 0.45359237;
+
+        System.out.print( "Introduce la altura en pulgadas: ");
+        final double alturaPulgadas = entrada.nextDouble();
+        final double alturaMetros = alturaPulgadas * 0.0254;
+
+
+        final double imc = pesoKg / Math.pow( alturaMetros, 2);
+        String salida = String.format( "Índice de masa corporal: %.4f", imc);
+        System.out.println( salida);
+	}
+}
