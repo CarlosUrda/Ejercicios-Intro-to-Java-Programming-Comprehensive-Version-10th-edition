eeb3f6c1f5488ef16dc0dac1c6567cb48a538e1d
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2016-01-07 19:59:35 +0100

    Implementación de los Ejercicios 22-27 del Capítulo 3 (El Ejercicio 22 está mejorado)

--- Capitulo 3/Ejercicio3_22.java
+++ Capitulo 3/Ejercicio3_22.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 /**
  * Ejercicio 22 del Capítulo 3: Comprobar si las coordenadas de un punto dado 
- * por el usuario están dentro de un círculo de centro (0,0) y radio 10.
+ * por el usuario están dentro de un círculo.
  *
  * @author Carlos A. Gómez Urda
  * @version 1.0
@@ -14,18 +14,23 @@ public class Ejercicio3_22
         // Locale.setDefault( Locale.ENGLISH);
         Scanner entrada = new Scanner( System.in).useLocale( Locale.ENGLISH);
 
-        System.out.print( "Introduce las coordenadas X e Y: ");
-        double x = entrada.nextDouble();
-        double y = entrada.nextDouble();
-    
-        final double radio = 10;
+        System.out.print( "Introduce las coordenadas X e Y del punto: ");
+        double puntoX = entrada.nextDouble();
+        double puntoY = entrada.nextDouble();
 
-        double distancia = Math.sqrt( Math.pow( x, 2) + Math.pow( y, 2));
+        System.out.print( "Introduce las coordenadas X e Y del centro: ");
+        double centroX = entrada.nextDouble();
+        double centroY = entrada.nextDouble();
 
-        if (distancia > radio)
-            System.out.println( "El punto está fuera");
-        else
-            System.out.println( "El punto está dentro");
+        System.out.print( "Introduce el radio: ");
+        double radio = entrada.nextDouble();
+
+        double distancia = Math.sqrt( Math.pow( puntoX - centroX, 2) + 
+                                      Math.pow( puntoY - centroY, 2));
+
+        System.out.printf( "El punto (%.3f,%.3f) " + 
+                           (distancia > radio ? "no" : "sí") + " está en el" + 
+                           " círculo con radio %.3f\n", puntoX, puntoY, radio);
 
     }
 }
