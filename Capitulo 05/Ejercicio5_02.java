36196d7f8ce66b5a32c500434364214e3365783d
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2016-02-15 23:54:11 +0100

    Intento de implementación del Ejercicio 2 del Capítulo 5

---
+++ Capitulo 05/Ejercicio5_02.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+/**
+ * Ejercicio 02 del Capítulo 5:
+ *
+ * @author Carlos A. Gómez Urda
+ * @version 1.0
+ */
+public class Ejercicio5_02
+{
+    public static void main( String[] args)
+    {
+        // Locale.useDefault( Locale.ENGLISH);
+        Scanner sc = new Scanner( System.in).useLocale( Locale.ENGLISH);
+
+    }
+ 
