43f9d6c1020abda60625d65696926787773f13c3
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2016-02-28 23:54:01 +0100

    Implementación del Ejercicio 10 del Capítulo 5

---
+++ Capitulo 05/Ejercicio5_10.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+/**
+ * Ejercicio 10 del Capítulo 5:
+ *
+ * @author Carlos A. Gómez Urda
+ * @version 1.0
+ */
+public class Ejercicio5_10
+{
+    public static void main( String[] args)
+    {
+        Locale.setDefault( Locale.ENGLISH);
+        Scanner sc = new Scanner( System.in);//.useLocale( Locale.ENGLISH);
+
+        System.out.print( "Introduce el límite inferior: ");
+        int inferior = sc.nextInt();
+
+        System.out.print( "Introduce el límite superior: ");
+        int superior = sc.nextInt();
+
+        int porLinea = 0;
+        for (int i = inferior; i <= superior; i++)
+        {
+            if (!((i%5 == 0) ^ (i%6 == 0))) continue;
+ 
+            System.out.print( i);
+            System.out.print( (++porLinea%10 == 0) ? "\n" : " ");
+        }
+    }
+}
+
+
+
